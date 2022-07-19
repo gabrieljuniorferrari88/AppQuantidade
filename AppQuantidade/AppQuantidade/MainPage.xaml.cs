1cdab46a90b8832a6bb473c8ef6338d106283b8f
Gabriel Ferrari <gabrieljuniorferrari88@gmail.com>
2022-07-19 07:56:44 -0300

    Adicionar arquivos de projeto.

---
+++ AppQuantidade/AppQuantidade/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppQuantidade
+{
+    public partial class MainPage : ContentPage
+    {
+        private int quantidade = 1;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void HandleIncrease(object sender, EventArgs e)
+        {
+            quantidade++;
+            LblQuantidade.Text = quantidade.ToString();
+        }
+
+        private void HandleDecrease(object sender, EventArgs e)
+        {
+           if(quantidade != 0)
+            {
+                quantidade--;
+                LblQuantidade.Text = quantidade.ToString();
+            }
+        }
+    }
+}
